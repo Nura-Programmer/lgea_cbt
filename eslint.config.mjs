58ed9899414987dee4474e5ffeeeb691a5ae785d
Nura Programmer <nuraprogrammer@gmail.com>
2025-07-18 01:49:58 +0100

    Fix eslint error: Error: A `require()` style import is forbidden

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["node_modules/**", ".next/**", "lib/generated/prisma/**"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
